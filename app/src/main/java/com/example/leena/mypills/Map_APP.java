b5787382a14426ff8c32fccc186442d7f51ed9f1
Leena <leena_bhandari@ymail.com>
2018-10-02 13:16:59 +0530

    Update Map_APP.java

--- app/src/main/java/com/example/leena/mypills/Map_APP.java
+++ app/src/main/java/com/example/leena/mypills/Map_APP.java
@@ -8,8 +8,8 @@ public class Map_APP extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        LicenceManager.getInstance().setRestAPIKey("v29j3asaqw97i6l6gvk4mnppon8fsaqz");
-        LicenceManager.getInstance().setMapSDKKey("bbjdknymv7seoqvglfkje9928ggz2iyc");
+        LicenceManager.getInstance().setRestAPIKey("v29*********************qz");
+        LicenceManager.getInstance().setMapSDKKey("bbjd**********************2iyc");
 
     }
 
